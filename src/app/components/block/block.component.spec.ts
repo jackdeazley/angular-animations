4545183b9c43aba2ef0a8e31367e24c649ecb903
jackdeazley <jack.deazley@hotmail.co.uk>
2023-01-31 00:35:13 +0000

    uupdating svg function and adding unit test

--- src/app/components/block/block.component.spec.ts
+++ src/app/components/block/block.component.spec.ts
@@ -8,16 +8,40 @@ describe('BlockComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BlockComponent ]
-    })
-    .compileComponents();
+      declarations: [BlockComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BlockComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('BC001 - ngONInit() will generate random svg points', () => {
+    // arrange
+    const spyOnGenerateSvgPoints = spyOn(
+      component,
+      `generateRandomSvgPoints`
+    ).and.returnValue('svg points');
+    //act
+    component.ngOnInit();
+
+    //assert
+    expect(spyOnGenerateSvgPoints).toHaveBeenCalled();
+    expect(component.svgPoints).toEqual('svg points');
+  });
+
+  it('BC002 - generateRandomSvgPoints wil generate random svg path string', () => {
+    // arrange
+    const spyOnGenerateNumber = spyOn(
+      component,
+      `generateRandomNumber`
+    ).and.returnValue(50);
+    const svgPath = `M50.8,-16.3C57.3,3.3,47.9,50,50,50C13.4,52.5,-11.9,51.9,-28.5,39.7C-50,27.6,-52.9,3.9,-46.7,-15.3C-50,-50,-20.2,-49.5,1,-49.8C22.2,-50.1,50,-35.9,50.8,-16.3Z`;
+    // act
+    const result = component.generateRandomSvgPoints();
+
+    // assert
+    expect(spyOnGenerateNumber).toHaveBeenCalledTimes(5);
+    expect(result).toEqual(svgPath);
   });
 });
--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -25,6 +25,6 @@ export class BlockComponent implements OnInit {
     const pointD = this.generateRandomNumber(100);
     const pointE = this.generateRandomNumber(100);
 
-    return `M50.8,-16.3C57.3,3.3,47.9,${pointB},${pointA},${pointD}C13.4,52.5,-11.9,51.9,-28.5,39.7C-${pointB},27.6,-52.9,3.9,-46.7,-15.3C-${pointC},-${pointE},-20.2,-49.5,1,-49.8C22.2,-50.1,44.4,-35.9,50.8,-16.3Z`;
+    return `M50.8,-16.3C57.3,3.3,47.9,${pointB},${pointA},${pointD}C13.4,52.5,-11.9,51.9,-28.5,39.7C-${pointB},27.6,-52.9,3.9,-46.7,-15.3C-${pointC},-${pointE},-20.2,-49.5,1,-49.8C22.2,-50.1,${pointA},-35.9,50.8,-16.3Z`;
   }
 }
